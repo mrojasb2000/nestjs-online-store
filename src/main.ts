0cf3579fb4529d2f574bbca6e74a211f6823817f
Mauricio Rojas <mrojasb2000@gmail.com>
2022-02-19 00:16:29 -0300

    Added env variables

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from 'path';
 
 async function bootstrap() {
+  require('dotenv').config()
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname,'..','public'));
   app.setBaseViewsDir(join(__dirname,'..','views'));
   app.setViewEngine('hbs');
   
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
