fee1194ed8eba67791ecd6f29d0ea25107728441
Mauricio Rojas <mrojasb2000@gmail.com>
2022-03-07 12:45:02 -0300

    Added product show

--- src/Products/products.controller.ts
+++ src/Products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from "@nestjs/common";
+import { Controller, Get, Param, Render } from "@nestjs/common";
 
 @Controller("/products")
 export class ProductsController {
@@ -40,6 +40,19 @@ export class ProductsController {
     viewData["title"] = "Products - Online Store";
     viewData["subtitle"] = "List of products";
     viewData["products"] = ProductsController.products;
+    return {
+      viewData: viewData,
+    };
+  }
+
+  @Get("/:id")
+  @Render("products/show")
+  show(@Param() params) {
+    const product = ProductsController.products[params.id - 1];
+    const viewData = [];
+    viewData["title"] = product.name + " - Online Store";
+    viewData["subtitle"] = product.name + " - Product Information";
+    viewData["product"] = product;
     return {
       viewData: viewData,
     };
