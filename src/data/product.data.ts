0614def8d639e13aa6c776581a97e67eb85229d9
Mauricio Rojas <mrojasb2000@gmail.com>
2022-03-18 17:09:20 -0300

    Added product service

---
+++ src/data/product.data.ts
@@ -0,0 +1,6 @@
+/*
+INSERT INTO product (name, description, image, price) VALUES ('TV', 'Best TV', 'game.png', '1000');
+INSERT INTO product (name, description, image, price) VALUES ('iPhone', 'Best iPhone', 'safe.png', '999');
+INSERT INTO product (name, description, image, price) VALUES ('Chromecast', 'Best Chromecast', 'submarine.png', '30');
+INSERT INTO product (name, description, image, price) VALUES ('Glasses', 'Best Glasses', 'game.png', '100');
+*/
