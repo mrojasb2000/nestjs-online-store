b87b362dba044fd7bb32419b09b3aba652c161ae
mavro <mrojasb2000@gmail.com>
2022-03-19 10:17:48 -0300

    Added get products from database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ProductsController } from "./products/products.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Product } from "./models/product.entity";
+import { ProductService } from "./Products/products.service";
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([Product])],
   controllers: [AppController, ProductsController],
-  providers: [AppService],
+  providers: [AppService, ProductService],
 })
 export class AppModule {}
