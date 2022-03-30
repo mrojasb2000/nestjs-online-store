5ab47f6559433a40972d32fdd7e2b6e69eda3227
mavro <mrojasb2000@gmail.com>
2022-03-30 16:59:27 -0300

    Added getter and setter

--- src/models/product.entity.ts
+++ src/models/product.entity.ts
@@ -16,4 +16,44 @@ export class Product {
 
   @Column()
   price: number;
+
+  getId() {
+    return this.id;
+  }
+
+  setId(id: number) {
+    this.id = id;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  getDescription() {
+    return this.description;
+  }
+
+  setDescription(description: string) {
+    this.description = description;
+  }
+
+  getImage() {
+    return this.image;
+  }
+
+  setImage(image: string) {
+    this.image = image;
+  }
+
+  getPrice() {
+    return this.price;
+  }
+
+  setPrice(price: number) {
+    this.price = price;
+  }
 }
